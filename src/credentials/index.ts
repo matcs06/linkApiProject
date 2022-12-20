2a663e5d8ee61d79226d80cdd8b42f86ec007a83
Mateus Costa Silva <38388657+matcs06@users.noreply.github.com>
2022-12-20 18:06:19 -0300

    Update index.ts
    removing api keys

--- src/credentials/index.ts
+++ src/credentials/index.ts
@@ -1,9 +1,9 @@
-const pipederiveApiKey = '19cecb2f3b8e9541fa17c785b2f6e025c50cd138';
+const pipederiveApiKey = '';
 const blingApiKey =
-  '744469295cccd26d06cc63aab2c5301ab871926cc40c5e6f69669165cd83ac50431df898';
+  '';
 
-const databasePassword = 'mateus';
-const databaseUser = 'mateus';
+const databasePassword = '';
+const databaseUser = '';
 
 export default {
   pipederiveApiKey,
